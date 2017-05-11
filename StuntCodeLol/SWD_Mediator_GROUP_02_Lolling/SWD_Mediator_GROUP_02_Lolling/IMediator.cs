00e1af8334aeddc9f62514add9c71d0047f6d92c
Anders Birkelund <anderswiggers41@gmail.com>
2017-05-11 09:07:32 +0200

    deleted classes

---
+++ StuntCodeLol/SWD_Mediator_GROUP_02_Lolling/SWD_Mediator_GROUP_02_Lolling/IMediator.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SWD_Mediator_GROUP_02_Lolling
+{
+    public interface IMediator
+    {
+        void Text(string message, int recievingNumber, int sendingNumber);
+        void TextAll(string message, int sendingnumber);
+        void Call(int recievingNumber, int callingNumber);
+        void Register(Phone smartphone);
+
+    }
+}
---
+++ StuntCodeLol/SWD_Mediator_GROUP_02_Lolling/SWD_Mediator_GROUP_02_Lolling/RegularPhone.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace SWD_Mediator_GROUP_02_Lolling
+{
+    class RegularPhone : Phone
+    {
+        public RegularPhone(int _phoneNumber, IMediator _mediator) : base(_phoneNumber, _mediator)
+        {
+        }      
+        public override void Text(int phoneNumber, string text)
+        {
+            mediator.Text("Sent from RegualarPhone: "+text, phoneNumber, myPhoneNumber);
+        }
+        public override void TextAll(string message)
+        {
+            mediator.TextAll("Sent from RegualarPhone: " + message, myPhoneNumber);
+        }
+        
+       
+    }
+}
